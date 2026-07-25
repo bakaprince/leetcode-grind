02077d43f007bada5b1d89861c6bda0ab3584819
Sudeep Kushwaha <bakaprince.dev@gmail.com>
2026-07-25 20:35:48 +0530

    Time: 0 ms (100%), Space: 44.4 MB (93.08%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,13 +3,11 @@ class Solution {
 
         int n = nums.length;
 
-        // Step 1: Find the first decreasing element from the right
         int pivot = n - 2;
         while (pivot >= 0 && nums[pivot] >= nums[pivot + 1]) {
             pivot--;
         }
 
-        // Step 2: If a pivot exists, find the next greater element
         if (pivot >= 0) {
             int successor = n - 1;
             while (nums[successor] <= nums[pivot]) {
@@ -19,7 +17,6 @@ class Solution {
             swap(nums, pivot, successor);
         }
 
-        // Step 3: Reverse the suffix
         reverse(nums, pivot + 1, n - 1);
     }
 
