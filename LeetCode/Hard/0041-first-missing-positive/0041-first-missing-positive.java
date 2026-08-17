d428432a022b223481d313e26416ae6a18aa268c
Sudeep Kushwaha <bakaprince.dev@gmail.com>
2026-08-17 15:21:46 +0530

    Array 2 ms (35.87%) 71.5 MB (38.7%) 0041-first-missing-positive Hard

--- LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.java
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,21 +1,42 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-
-        if(nums.length==0){
-            return 0;
-        }
-        Map<Integer,Integer> map=new HashMap<>();
-
         int n=nums.length;
-        for(int num:nums){
-            if(num>=1&&num<=n){
-                map.put(num,num);
+        int contains=0;
+
+        for(int i=0;i<n;i++){
+            if (nums[i]==1){
+                contains++;
+                break;
             }
         }
-        for(int i=1;i<=n;i++){
-            if(!map.containsKey(i)){
+
+        if(contains==0){
+            return 1;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=0||nums[i]>n){
+                nums[i]=1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int a=Math.abs(nums[i]);
+
+            if(a==n){
+                nums[0]=-Math.abs(nums[0]);
+            }
+            else{
+                nums[a]=-Math.abs(nums[a]);
+            }
+        }
+        for(int i=1;i<n;i++){
+            if(nums[i]>0){
                 return i;
             }
+        }
+
+        if(nums[0]>0){
+            return n;
         }
         return n+1;
     }
