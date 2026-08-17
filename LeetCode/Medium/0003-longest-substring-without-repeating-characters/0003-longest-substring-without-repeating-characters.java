4c34316d2f847b7bb99aaf3569d7b766cefbd75c
Sudeep Kushwaha <bakaprince.dev@gmail.com>
2026-08-17 21:36:35 +0530

    Hash Table 62 ms (24.35%) 48 MB (8.69%) 0003-longest-substring-without-repeating-characters Medium

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,23 +1,29 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        HashSet<Character> set = new HashSet<>();
-
-        int left = 0;
-        int ans = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-
-            while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left));
-                left++;
-            }
-
-            set.add(s.charAt(right));
-
-            ans = Math.max(ans, right - left + 1);
+        if(s==null ||s.isEmpty()){
+            return 0;
         }
 
+        if(s.length()==1){
+            return 1;
+        }
+        
+        int left=0;
+        int right=0;
+        int ans=0;
+        Set<Character> set=new HashSet<>();
+
+        char[] c=s.toCharArray();
+
+        while(right<c.length){
+            while(set.contains(c[right])){
+                set.remove(c[left]);
+                left++;
+            }
+            set.add(c[right]);
+            ans=Math.max(ans,right-left+1);
+            right++;
+        }
         return ans;
     }
 }
