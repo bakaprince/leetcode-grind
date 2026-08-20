aac3b2573b90835b6e353e5da4e70c2248fd5de9
Sudeep Kushwaha <bakaprince.dev@gmail.com>
2026-08-20 19:24:16 +0530

    Array 449 ms (12.33%) 60.4 MB (7.44%) 0015-3sum Medium

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,52 +1,37 @@
 class Solution {
-
     public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> unique = new HashSet<>();
 
         Arrays.sort(nums);
 
-        List<List<Integer>> ans = new ArrayList<>();
-
         for (int i = 0; i < nums.length - 2; i++) {
-
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    left++;
-                    right--;
-
-                    while (left < right &&
-                           nums[left] == nums[left - 1])
-                        left++;
-
-                    while (left < right &&
-                           nums[right] == nums[right + 1])
-                        right--;
-
-                }
-
-                else if (sum < 0)
-                    left++;
-
-                else
-                    right--;
-
             }
 
+            int a = nums[i];
+
+            Set<Integer> set = new HashSet<>();
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                int b = nums[j];
+
+                int required = -a - b;
+
+                if (set.contains(required)) {
+
+                    List<Integer> triplet =
+                        Arrays.asList(a, required, b);
+
+                    unique.add(triplet);
+                }
+
+                set.add(b);
+            }
         }
 
-        return ans;
-
+        return new ArrayList<>(unique);
     }
-
 }
