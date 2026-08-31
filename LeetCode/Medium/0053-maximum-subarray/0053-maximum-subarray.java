2301151d46dabc86ae7ef00e08b184db22017951
Sudeep Kushwaha <bakaprince.dev@gmail.com>
2026-08-31 19:02:08 +0530

    Array 1 ms (99.94%) 77.2 MB (66.46%) 0053-maximum-subarray Medium

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
+        int max=Integer.MIN_VALUE;
+        int sum=0;
 
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+        for(int i=0;i<nums.length;i++){
+
+            sum+=nums[i];
+            max=Math.max(sum,max);
+            if(sum<0){
+                sum=0;
+            }
         }
+        return max;
 
-        return maxSum;
     }
 }
