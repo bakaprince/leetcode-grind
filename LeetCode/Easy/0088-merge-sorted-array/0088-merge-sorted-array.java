321c340bb7b9397fd9ea58a0d5c59e3d205c871a
Sudeep Kushwaha <bakaprince.dev@gmail.com>
2026-09-02 19:18:21 +0530

    Array 0 ms (100%) 43.9 MB (47.66%) 0088-merge-sorted-array Easy

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index=0;
-        for(int i=m;i<nums1.length;i++){
-            nums1[i]=nums2[index];
-            index++;
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+
+        while (j >= 0) {
+
+            if (i < 0) {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            else if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;    
         }
-        Arrays.sort(nums1);
+
     }
 }
